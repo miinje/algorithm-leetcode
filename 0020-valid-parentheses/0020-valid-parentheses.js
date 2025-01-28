c7388344fab4b6a7ac4e59d816a7168d52fbb587
minjee <njjj00083@gmail.com>
2025-01-28 19:28:21 +0900

    Time: 10 ms (8.59%), Space: 50.6 MB (91.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const brackets = {
+        "[" : "]",
+        "{" : "}",
+        "(" : ")",
+    }; 
+    const stack = [];
+
+    if (s.length % 2 !== 0) {
+        return false;
+    }
+
+    for (bracket of s) {
+        if (Object.hasOwn(brackets, bracket)) {
+            stack.unshift(bracket);
+        } else {
+            if (brackets[stack[0]] === bracket) {
+                stack.shift();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
